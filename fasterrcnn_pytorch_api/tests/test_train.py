02d8642edbc3e04410f9189b6d5dacc4e4bd851f
khadijeh.alibabaei <khadijeh.alibabaei@kit.edu>
2023-05-23 10:13:25 +0200

    add existing prject file to git

--- fasterrcnn_pytorch_api/tests/test_train.py
+++ fasterrcnn_pytorch_api/tests/test_train.py
@@ -1,4 +1,4 @@
-#import os
+import os
 
-#def test_train_function(trained_model_path):
-  #  assert 'best_model.pth' in os.listdir(trained_model_path), f"No checkpoint was saved"
+def test_train_function(trained_model_path):
+    assert 'best_model.pth' in os.listdir(trained_model_path), f"No checkpoint was saved"
