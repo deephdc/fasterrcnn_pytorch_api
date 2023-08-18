d8bdaaf95a241991e80a4ecc651479a10dadff0a
khadijeh.alibabaei <khadijeh.alibabaei@kit.edu>
2023-08-18 14:58:59 +0000

    update

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,4 +1,8 @@
 import os
+import shutil
 def test_train_function(trained_model_path):
-   assert  'last_model.pth' in os.listdir(trained_model_path), "No checkpoint was saved"
-   os.rmdir(trained_model_path)
+   checkpoint_file = os.path.join(trained_model_path, 'last_model.pth')
+   assert 'last_model.pth' in os.listdir(trained_model_path), "No checkpoint was saved"
+   assert os.path.exists(checkpoint_file), "last_model.pth does not exist"
+    # Remove the saved model directory and its contents
+   shutil.rmtree(trained_model_path)
